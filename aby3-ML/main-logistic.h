4f2050a0ada4b1c27ed061c034e51fff225a7c63
Mazharul Islam <mislam9@wisc.edu>
2022-08-02 15:31:35 -0700

    Removing DB code

--- aby3-ML/main-logistic.h
+++ aby3-ML/main-logistic.h
@@ -4,4 +4,8 @@ namespace aby3
 {
 	int logistic_main_3pc_sh(oc::CLP& cmd);
 	int logistic_plain_main(oc::CLP& cmd);
+
+    //template<Decimal  D>
+    //using sf64Matrix = sfMatrix<i64, D>;
+
 }
--- frontend/main.cpp
+++ frontend/main.cpp
@@ -2,9 +2,6 @@
 #include <cryptoTools/Common/CLP.h>
 
 #include "aby3_tests/aby3_tests.h"
-#include "eric.h"
-#include "aby3-DB-main.h"
-#include "aby3-DB_tests/UnitTests.h"
 #include <tests_cryptoTools/UnitTests.h>
 #include <aby3-ML/main-linear.h>
 #include <aby3-ML/main-logistic.h>
@@ -25,9 +22,6 @@ void help()
 	std::cout << "-u                        ~~ to run all tests" << std::endl;
 	std::cout << "-u n1 [n2 ...]            ~~ to run test n1, n2, ..." << std::endl;
 	std::cout << "-u -list                  ~~ to list all tests" << std::endl;
-	std::cout << "-intersect -nn NN [-c C]  ~~ to run the intersection benchmark with 2^NN set sizes, C 32-bit data columns." << std::endl;
-	std::cout << "-eric -nn NN              ~~ to run the eric benchmark with 2^NN set sizes" << std::endl;
-	std::cout << "-threat -nn NN -s S       ~~ to run the threat log benchmark with 2^NN set sizes and S sets" << std::endl;
 }
 
 
@@ -46,8 +40,6 @@ int main(int argc, char** argv)
 		{
 			auto tests = tests_cryptoTools::Tests;
 			tests += aby3_tests;
-			tests += DB_tests;
-
 			tests.runIf(cmd);
 			return 0;
 		}
@@ -80,85 +72,6 @@ int main(int argc, char** argv)
 			neural_pred_main_3pc_sh(cmd);
 		}
 
-		if (cmd.isSet("eric"))
-		{
-			set = true;
-
-			auto nn = cmd.getMany<int>("nn");
-			if (nn.size() == 0)
-				nn.push_back(16);
-
-			for (auto n : nn)
-			{
-				eric(1 << n);
-			}
-		}
-
-
-		if (cmd.isSet("intersect"))
-		{
-			set = true;
-
-			auto nn = cmd.getMany<int>("nn");
-			auto c = cmd.getOr("c", 0);
-			if (nn.size() == 0)
-				nn.push_back(1 << 16);
-
-			for (auto n : nn)
-			{
-				auto size = 1 << n;
-				DB_Intersect(size, c, cmd.isSet("sum"));
-			}
-		}
-
-
-		if (cmd.isSet("threat"))
-		{
-			set = true;
-
-			auto nn = cmd.getMany<int>("nn");
-			auto c = cmd.getOr("s", 2);
-			if (nn.size() == 0)
-				nn.push_back(1 << 16);
-
-			for (auto n : nn)
-			{
-				auto size = 1 << n;
-				DB_threat(size, c);
-			}
-		}
-
-
-
-		if (cmd.isSet("card"))
-		{
-			set = true;
-
-			auto nn = cmd.getMany<int>("nn");
-			if (nn.size() == 0)
-				nn.push_back(1 << 16);
-
-			for (auto n : nn)
-			{
-				auto size = 1 << n;
-				DB_cardinality(size);
-			}
-		}
-		
-		//if (cmd.isSet("add"))
-		//{
-		//	set = true;
-
-		//	auto nn = cmd.getMany<int>("nn");
-		//	if (nn.size() == 0)
-		//		nn.push_back(1 << 16);
-
-		//	for (auto n : nn)
-		//	{
-		//		auto size = 1 << n;
-		//		Sh3_add_test(size);
-		//	}
-		//}
 
 		if (set == false)
 		{
